1a4da9733351bf2d6ef7858ee24a0b0725dd75fc
giorgiiakobidze <levaniakobidze25@gmail.com>
2025-01-15 18:06:49 +0400

    add booking page

--- src/script.js
+++ src/script.js
@@ -557,10 +557,15 @@ window.formSubmitHandler = (token) => {
 			params:{pro_id: pro_id || null }	
 		})
 		.then((data) => {
+			console.log(data,'dataa');
+			
 			// Reset recaptcha
 			grecaptcha.reset();
 			if (!redirect_to) {
-				return 	window.location.href = `https://app.unclekam.com/thankyou?uuid=${data?.data?.data?.lead_uuid}&appointment=${data?.data?.data?.allow_appointments}`;
+				return data?.data?.data?.allow_appointments ?
+				window.location.href = `${Env && Env === 'true' ? 'https://staging.unclekam.com/' : 'https://app.unclekam.com'}/book-appointment?lead=${data?.data?.data?.lead_uuid}&customer=${data?.data?.data?.user_uuid}&operator=${data?.data?.data?.userpro_uuid}`
+				:
+				window.location.href = `${Env && Env === 'true' ? 'https://staging.unclekam.com/' : 'https://app.unclekam.com'}https://app.unclekam.com/thankyou?uuid=${data?.data?.data?.lead_uuid}&appointment=false` 
 			}
 			// Redirect to given url if needed
 			if (redirect_to) {
